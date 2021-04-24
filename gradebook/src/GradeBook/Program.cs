e7f6613ebc7439769e14a503c0ee2343b9ff1fc1
Sean Edwards <seantrevoredwards@gmail.com>
2021-04-24 06:48:39 +0100

    First array assigned and summed

--- gradebook/src/GradeBook/Program.cs
+++ gradebook/src/GradeBook/Program.cs
@@ -6,11 +6,16 @@ namespace GradeBook
     {
         static void Main(string[] args)
         {   
-            var x = 34.1;
-            var y = 2.56;
-            var result = y + x;
-            Console.WriteLine($"The result of {y} + {x} is {result}");
+            var numbers = new double[3];
+            numbers[0] = 12.7;
+            numbers[1] = 1.2;
+            numbers[2] = 7.1;
+            var result = numbers[0] + numbers[1] + numbers[2];
+            Console.WriteLine(result);
             
+
+
+
             if(args.Length > 0)
             {
                 Console.WriteLine($"Hello, {args[1]}!");
