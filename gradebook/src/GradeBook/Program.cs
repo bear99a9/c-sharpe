05a66c095c1a03d2d860805e6cd999567670c722
Sean Edwards <seantrevoredwards@gmail.com>
2021-04-24 07:05:25 +0100

    Added a new List

--- gradebook/src/GradeBook/Program.cs
+++ gradebook/src/GradeBook/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace GradeBook
 {
@@ -7,6 +8,7 @@ namespace GradeBook
         static void Main(string[] args)
         {   
             var numbers = new[] {12.7, 10.3, 6.11, 4.11};
+            List grades;
             var result = 0.0;
             foreach (var number in numbers)
             {
