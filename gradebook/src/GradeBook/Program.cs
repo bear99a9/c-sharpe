f64f2b00472b892b802cd3b2c9554b8324f29e97
Sean Edwards <seantrevoredwards@gmail.com>
2021-04-24 06:37:40 +0100

    First variable delcared and addition used

--- gradebook/src/GradeBook/Program.cs
+++ gradebook/src/GradeBook/Program.cs
@@ -5,8 +5,13 @@ namespace GradeBook
     class Program
     {
         static void Main(string[] args)
-        {
-            if(args.Length > 0) 
+        {   
+            double x = 34.1;
+            double y = 2.56;
+            double result = y + x;
+            Console.WriteLine($"{result}");
+
+            if(args.Length > 0)
             {
                 Console.WriteLine($"Hello, {args[1]}!");
             }
