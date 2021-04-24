160364e342ac68c2a6e770d473605b1032df8076
Sean Edwards <seantrevoredwards@gmail.com>
2021-04-24 07:07:21 +0100

    Foreach loop over new grades List

--- gradebook/src/GradeBook/Program.cs
+++ gradebook/src/GradeBook/Program.cs
@@ -8,9 +8,12 @@ namespace GradeBook
         static void Main(string[] args)
         {   
             var numbers = new[] {12.7, 10.3, 6.11, 4.11};
-            List grades;
+
+            var grades = new List<double>() {12.7, 10.3, 6.11, 4.11};
+            grades.Add(56.1);
             var result = 0.0;
-            foreach (var number in numbers)
+
+            foreach (var number in grades)
             {
                 result += number;
             }
