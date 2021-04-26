19cd13524441f065befab97292c5d4313a156f71
Sean Edwards <seantrevoredwards@gmail.com>
2021-04-26 17:24:26 +0100

    Added gloobal variable grades

--- gradebook/src/GradeBook/Book.cs
+++ gradebook/src/GradeBook/Book.cs
@@ -1,11 +1,15 @@
+using System.Collections.Generic;
+
 namespace GradeBook 
 {
     
     class Book
-    {
+    {   
+        List<double> grades; 
         public void AddGrade(double grade) 
         {
-            
+            grades.Add(grade);
         } 
+
     }
 }
