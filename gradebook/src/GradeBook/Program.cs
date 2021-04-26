e31b42313465f3f1810c36f0b29c73b8bbd7cea3
Sean Edwards <seantrevoredwards@gmail.com>
2021-04-26 14:22:13 +0100

    Removed if hello statement

--- gradebook/src/GradeBook/Program.cs
+++ gradebook/src/GradeBook/Program.cs
@@ -19,13 +19,6 @@ namespace GradeBook
             result /= grades.Count;
             Console.WriteLine($"The average grade is {result:N1}");
             
-            if(args.Length > 0)
-            {
-                Console.WriteLine($"Hello, {args[1]}!");
-            }
-            else {
-                Console.WriteLine("Hello!");
-            }
         }
     }
 }
