80842b1282fde65c301e85310b8720648ea905a3
Sean Edwards <seantrevoredwards@gmail.com>
2021-04-30 18:59:13 +0100

    First xunit test

--- gradebook/test/GradeBook.Tests/UnitTest1.cs
+++ gradebook/test/GradeBook.Tests/UnitTest1.cs
@@ -7,8 +7,12 @@ namespace GradeBook.Tests
     {
         [Fact]
         public void Test1()
-        {
-
+        {   
+            var x = 5;
+            var y = 2;
+            var expected = 10;
+            var actual = x * y;
+            Assert.Equal(expected, actual);
         }
     }
 }
