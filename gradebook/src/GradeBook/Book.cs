934f607ba9321566187cc46399dfca407bf2136e
Sean Edwards <seantrevoredwards@gmail.com>
2021-04-30 18:38:40 +0100

    Refactored Program class to takke out logic into Book class

--- gradebook/src/GradeBook/Book.cs
+++ gradebook/src/GradeBook/Book.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System;
 
 namespace GradeBook 
 {
@@ -11,10 +12,43 @@ namespace GradeBook
             this.name = name; 
         }
 
+        public void ShowStats() {
+            HighGrade();
+            LowGrade();
+            AverageGrade(); 
+        }
+
         public void AddGrade(double grade) 
         {
             grades.Add(grade);
         } 
+
+        private void HighGrade()
+        {
+            grades.Sort();
+            var high = grades[^1];
+            Console.WriteLine($"The highest grade is {high}");
+        }
+
+        private void LowGrade()
+        {
+            grades.Sort();
+            var low = grades[0];
+            Console.WriteLine($"The lowest grade is {low}");
+        }
+
+        private void AverageGrade() 
+        {
+            var result = 0.0;
+
+            foreach (var number in grades)
+            {   
+                result += number;
+            }
+            result /= grades.Count;
+            Console.WriteLine($"The average grade is {result:N1}");
+        }
+
         private List<double> grades; 
         private string name; 
 
--- gradebook/src/GradeBook/Program.cs
+++ gradebook/src/GradeBook/Program.cs
@@ -9,40 +9,9 @@ namespace GradeBook
         {   
             var book = new Book("Sean's grade book");
             book.AddGrade(89.1);
-            book.AddGrade(93.1);
-            book.AddGrade(9.7);
-
-
-            var grades = new List<double>() {12.7, 10.3, 6.11, 4.11};
-            grades.Add(56.1);
-            grades.Sort();
-            var low = grades[0]; 
-            grades.Reverse();
-            var high = grades[0];
-            
-
-            var result = 0.0;
-            var highGrade = double.MinValue;
-            var lowGrade = double.MaxValue; 
-
-            foreach (var number in grades)
-            {   
-                highGrade = Math.Max(number, highGrade);
-                lowGrade = Math.Min(number, lowGrade); 
-                result += number;
-            }
-            result /= grades.Count;
-            Console.WriteLine($"The average grade is {result:N1}");
-            if (high == highGrade) 
-            {
-                Console.WriteLine($"The highest grade is {high}");
-            }
-            if (low == lowGrade) 
-            {
-                Console.WriteLine($"The lowest grade is {low}");
-
-            }
-
+            book.AddGrade(90.5);
+            book.AddGrade(77.5);
+            book.ShowStats();
         }
     }
 }
