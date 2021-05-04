a573e067c2b43c6e419cf17e4c421c58364fb639
Sean Edwards <seantrevoredwards@gmail.com>
2021-05-04 08:50:39 +0100

    Refactored TestType into object test

--- gradebook/test/GradeBook.Tests/TypeTest.cs
+++ gradebook/test/GradeBook.Tests/TypeTest.cs
@@ -6,14 +6,26 @@ namespace GradeBook.Tests
     public class TypeTests
     {
         [Fact]
-        public void TypeTest()
+        public void GetBookReturnsDifferentObjects()
         {   
             var book1 = GetBook("Book 1");
             var book2 = GetBook("Book 2");
 
             Assert.Equal("Book 1", book1.Name);
             Assert.Equal("Book 2", book2.Name);
+            Assert.NotSame(book1, book2);
         }
+
+        [Fact]
+        public void TwoVarsCanReferenceSameObjects()
+        {   
+            var book1 = GetBook("Book 1");
+            var book2 = book1;
+
+            Assert.Same(book1, book2);
+            Assert.True(Object.ReferenceEquals(book1, book2));
+        }
+
 
         Book GetBook(string name)
         {
