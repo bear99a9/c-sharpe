62cec74ca9367f90eeb84b649590375094fdc65f
Sean Edwards <seantrevoredwards@gmail.com>
2021-05-04 08:33:16 +0100

    Update book class Name variable

--- gradebook/src/GradeBook/Book.cs
+++ gradebook/src/GradeBook/Book.cs
@@ -9,7 +9,7 @@ namespace GradeBook
         public Book(string name) 
         {
             grades = new List<double>();
-            this.name = name; 
+            Name = name; 
         }
 
         public Statistics GetStatistics() {
@@ -35,7 +35,7 @@ namespace GradeBook
         }
 
         private List<double> grades; 
-        private string name; 
+        public string Name; 
 
     }
 }
