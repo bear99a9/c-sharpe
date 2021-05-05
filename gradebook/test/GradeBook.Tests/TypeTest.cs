3fbcd46dfdf400df5a062307c3fae0c797a52114
Sean Edwards <seantrevoredwards@gmail.com>
2021-05-05 14:47:35 +0100

    Added C# pass by value test

--- gradebook/test/GradeBook.Tests/TypeTest.cs
+++ gradebook/test/GradeBook.Tests/TypeTest.cs
@@ -5,7 +5,21 @@ namespace GradeBook.Tests
 {
     public class TypeTests
     {
-    [   Fact]
+        [Fact]
+        public void CSHARPIsPassByValue()
+        {   
+            var book1 = GetBook("Book 1");
+            GetBookSetName(book1, "New Name");
+
+            Assert.Equal("Book 1", book1.Name);
+        }
+
+        private void GetBookSetName(Book book, string name)
+        {   
+            book = new Book(name);
+        }
+
+        [Fact]
         public void SetNameFromReference()
         {   
             var book1 = GetBook("Book 1");
