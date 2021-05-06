d3581dbc66a6aed7b0962b981bbd9e6e883f9a51
Sean Edwards <seantrevoredwards@gmail.com>
2021-05-06 14:02:21 +0100

    Extracted out Book class into Book.cs and renamed Book.cs to InMemoryBook.cs

--- gradebook/src/GradeBook/Book.cs
+++ gradebook/src/GradeBook/Book.cs
@@ -1,10 +1,5 @@
-using System.Collections.Generic;
-using System;
-
-namespace GradeBook 
-{   
-    public delegate void GradeAddedDelegate(object sender, EventArgs args); 
-
+namespace GradeBook
+{
     public abstract class Book :NamedObject
     {
         public Book(string name) : base(name)
@@ -12,80 +7,5 @@ namespace GradeBook
         }
 
         public abstract void AddGrade(double grade);
-    }
-    public class InMemoryBook : Book
-    {   
-        public InMemoryBook(string name) : base(name)
-        {   
-            grades = new List<double>();
-            Name = name; 
-        }
-
-        public Statistics GetStatistics() {
-            
-            var result = new Statistics();
-            result.Average = 0.0;
-            grades.Sort();
-            result.High = grades[^1];
-            result.Low = grades[0];
-            
-            foreach (var grade in grades)
-            {   
-                if(grade == 42.1) 
-                {
-                    continue;
-                }
-                result.Average += grade;
-            }
-
-            result.Average /= grades.Count;
-
-            switch (result.Average)
-            {
-                case var d when d >= 90.0:
-                    result.Letter = 'A';
-                    break;
-                case var d when d >= 80.0:
-                    result.Letter = 'B';
-                    break;
-                case var d when d >= 70.0:
-                    result.Letter = 'C';
-                    break;
-                case var d when d >= 60.0:
-                    result.Letter = 'D';
-                    break;
-                case var d when d >= 50.0:
-                    result.Letter = 'E';
-                    break;
-                default:
-                    result.Letter = 'F';
-                    break;
-            }
-            
-            return result;
-        }
-        public override void AddGrade(double grade) 
-        {   
-            if (grade <= 100 && grade >= 0)
-            {   
-                grades.Add(grade); 
-                if(GradeAdded != null)
-                {
-                    GradeAdded(this, new EventArgs());
-                }
-            }
-            else
-            {
-                throw new ArgumentException($"Invalid {nameof(grade)}");
-            }
-        }
-
-        public event GradeAddedDelegate GradeAdded;
-
-        private List<double> grades; 
-        
-        public const string CATEGORY = "Science";
-        // const cannot be overwritten anywhere 
-        // public allows the const to be read from outside the class
     }
 }
